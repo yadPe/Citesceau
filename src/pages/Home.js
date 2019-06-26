b23cedcb06d685d3d8dfc617fe1aa0a6ed8ba794
Virgile Vercellis <vmv92i@hotmail.fr>
2019-06-26 22:54:34 +0200

    Creation des composants

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../Home.css';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   constructor(props) {
@@ -12,9 +13,13 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1> Citoyen </h1>
-        <Button color="primary" size="lg" className="PropositionProjet">Proposition Projet</Button>
-        <Button color="primary" size="lg" className="ConsultationProjet">Consultation Projets</Button>
+        <h1 className="Citoyen"> Citoyen </h1>
+        <Link to="/NewProjet">
+          <Button color="primary" size="lg" className="PropositionProjet">Proposition Projet</Button>
+        </Link>
+        <Link to="/VueEnsemble">
+          <Button color="primary" size="lg" className="ConsultationProjet">Consultation Projets</Button>
+        </Link>
       </div>
     );
   }
