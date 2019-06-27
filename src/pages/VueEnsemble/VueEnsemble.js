eb1a4b31e97119298883ff0b5d49846bca906249
Kevin <kevcass02@gmail.com>
2019-06-27 12:08:12 +0200

    Creation and addition of the component Header.js.

--- src/pages/VueEnsemble/VueEnsemble.js
+++ src/pages/VueEnsemble/VueEnsemble.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import Header from '../Header';
 import NavBar from '../NavBar';
 
 class VueEnsemble extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
-    render() {
-        return (
-            <div>
-                <NavBar />
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <NavBar />
+      </div>
+    );
+  }
 }
 
 export default VueEnsemble;
