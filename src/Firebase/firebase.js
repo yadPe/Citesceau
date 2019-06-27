705a0e4543a366e60fa6e8c1f24ab8d970e6edb9
Justine Vareille <vareille.justine@gmail.com>
2019-06-27 17:17:16 +0200

    je sais pas

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const config = {
-  apiKey: "AIzaSyBle4csTAAOWsDXr8jHSX8YyRZBI3yJU98",
-  authDomain: "hackatown-645d6.firebaseapp.com",
-  databaseURL: "https://hackatown-645d6.firebaseio.com",
-  projectId: "hackatown-645d6",
-  storageBucket: "hackatown-645d6.appspot.com",
-  messagingSenderId: "723473958065",
-  appId: "1:723473958065:web:0835ff1fc4b7e666"
+  apiKey: 'AIzaSyBle4csTAAOWsDXr8jHSX8YyRZBI3yJU98',
+  authDomain: 'hackatown-645d6.firebaseapp.com',
+  databaseURL: 'https://hackatown-645d6.firebaseio.com',
+  projectId: 'hackatown-645d6',
+  storageBucket: 'hackatown-645d6.appspot.com',
+  messagingSenderId: '723473958065',
+  appId: '1:723473958065:web:0835ff1fc4b7e666',
 };
 
 class Firebase {
@@ -22,20 +22,18 @@ class Firebase {
     this.storage = app.storage();
   }
 
-  user = (id) => this.db.collection('Users')
+  user = id => this.db.collection('Users')
     .doc(id)
     .get()
     .then(querySnapshot => ({ ...querySnapshot.data(), id: querySnapshot.id }));
 
-  newUser = (user) => this.db.doc(`Users/${user.uid}`).set({ ...user, points: 100 }, { merge: true });
+  newUser = user => this.db.doc(`Users/${user.uid}`).set({ ...user, points: 100 }, { merge: true });
 
   projets = () => this.db.collection('Projets')
     .get()
     .then(querySnapshot => querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
 
-  newProjet = (projet) => this.db.collection(`Projets`).add({ ...projet, creationDate: new Date(), points: 0 });
-
-
+  newProjet = projet => this.db.collection('Projets').add({ ...projet, creationDate: new Date(), points: 0 });
 }
 
 export default Firebase;
